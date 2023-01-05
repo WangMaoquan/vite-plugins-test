8b2d6928cd2fa7b88f41fccd43158de18f0a5bf8
wangmaoquan <1782976211@qq.com>
2023-01-05 15:45:24 +0800

    feat: 使用@vitejs/plugin-legacy

---
+++ polyfill.md
@@ -0,0 +1,17 @@
+### Vite 语法降级与 Polyfill 注入
+
+Vite 官方已经为我们封装好了一个开箱即用的方案: `@vitejs/plugin-legacy`，我们可以基于它来解决项目语法的浏览器兼容问题。这个插件内部同样使用 @babel/preset-env 以及 core-js 等一系列基础库来进行语法降级和 Polyfill 注入
+
+```
+pnpm i @vitejs/plugin-legacy -D
+```
+
+在 `vite.config.ts` 使用
+
+```
+pnpm run build
+```
+
+通过官方的 `legacy` 插件， Vite 会分别打包出 `Modern` 模式和 `Legacy` 模式的产物，然后将两种产物插入同一个 HTML `里面，Modern` 产物被放到 `type="module"`的 script 标签中，而 `Legacy` 产物则被放到带有 `nomodule` 的 `script` 标签中
+
+### 插件执行原理
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { vitePluginTestHook, loadVirtualModule } from './plugins';
-
+import legacy from '@vitejs/plugin-legacy';
 // https://vitejs.dev/config/
 export default defineConfig({
   // plugins: [vue(), vitePluginTestHook(), loadVirtualModule()],
+  plugins: [
+    legacy({
+      // 设置目标浏览器，browserslist 配置语法
+      // 这个参数在插件内部会透传给@babel/preset-env
+      targets: ['ie >= 11'],
+    }),
+  ],
 });
